b92991e966cdf830ba091e8780da8318d7f2ed36
Mateus Cançado <mateuslimacancado55@gmail.com>
2026-06-10 15:52:28 -0300

    findById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,24 +1,18 @@
 package application;
 
-import java.util.Date;
-
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Department dp = new Department(2, "Books");
-		System.out.println(dp);
-		
-		Seller seller = new Seller(5, "Mateus", "mateus@gmail.com", new Date(), 3000.0, dp);
-		System.out.println(seller);
-		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
+		Seller seller = sellerDao.findById(3);
+		
+		System.out.println(seller);
 
 	}
 
