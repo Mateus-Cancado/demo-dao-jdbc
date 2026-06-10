768e83bba7316f423ad01ca86fb75d5d9d39e182
Mateus Cançado <mateuslimacancado55@gmail.com>
2026-06-10 14:59:36 -0300

    Department entity class

---
+++ src/application/Program.java
@@ -0,0 +1,14 @@
+package application;
+
+import model.entities.Department;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Department dp = new Department(2, "Books");
+		System.out.println(dp);
+
+	}
+
+}
